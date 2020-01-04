5ac553fddd0bbf750918809de85528cb1c66583a
Minh Quan <48386342+artiliusjoker@users.noreply.github.com>
2020-01-04 23:10:25 +0700

    Fix view profile admin

--- models/admin.js
+++ models/admin.js
@@ -1,28 +1,31 @@
 const mongoose = require('mongoose');
-const {Schema} = mongoose;
+const { Schema } = mongoose;
 
 const adminSchema = new Schema({
-	email: String,
 	username: String,
 	password: String,
+	email: String,
 	fullname: String,
-	phone: String,
 	birthday: Date,
 	avatar: String,
 	address: {
-		type : String,
-		default : 'Ho Chi Minh University of Science'
+		type: String,
+		default: 'Ho Chi Minh University of Science'
+	},
+	phone: {
+		type: String,
+		default: 'Chưa có'
 	},
 	role: {
-		type : String,
-		enum : ['normal', 'super', 'editor', 'monitor'],
-		default : 'normal'
+		type: String,
+		enum: ['normal', 'super', 'editor', 'monitor'],
+		default: 'normal'
 	},
 	status: {
-		type : String,
-		enum : ['active', 'blocked', 'deleted'],
-		default : 'active'
+		type: String,
+		enum: ['active', 'blocked', 'deleted'],
+		default: 'active'
 	}
-});
+})
 
 module.exports = mongoose.model('admin', adminSchema, 'admin');
