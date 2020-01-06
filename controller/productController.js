94debbe19c7bf2d9adc892377dbf26410874e4c6
Minh Quan <48386342+artiliusjoker@users.noreply.github.com>
2020-01-06 20:00:26 +0700

    commit before merge with develop

--- controller/productController.js
+++ controller/productController.js
@@ -1,12 +1,22 @@
 const ejsHelper = require('../views/helpers/helper');
+const productService = require('../models/productService');
+const productViewService = require('../models/productViewService');
+
 var productController = {};
 
 productController.getProductList = (req, res, next) => {
     res.render('pages/product/list', { title: 'Products', name: 'Products List' });
 }
 
-productController.getProductAdd = (req, res, next) => {
-    res.render('pages/product/add', { title: 'Add product', name: 'Product/Add' });
+productController.getProductAdd = async (req, res, next) => {
+    const productsMetaData = await productService.getDataOfProduct();
+    const wrappedMetaData = await productViewService.wrapData(productsMetaData);
+    console.log(wrappedMetaData);
+    const viewData = {
+        helper : 'null',
+        metadata : wrappedMetaData
+    }
+    res.render('pages/product/add', { title: 'Add product', name: 'Add Product', viewData : viewData });
 }
 
 productController.postProductAdd = (req, res, next) => {
